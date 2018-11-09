10f7dfc740ba836ce484999076fa328c99abb506
politrons <politrons@gmail.com>
2018-11-09 22:34:25 +0100

    Add Scotty library

--- src/main/eta/ScottyHttpServer.hs
+++ src/main/eta/ScottyHttpServer.hs
@@ -6,7 +6,7 @@ import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON, encode,decode)
 import GHC.Generics
 import ModelTypes
-import MySQLConnector
+--import MySQLConnector
 
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Web.Scotty.Internal.Types (ScottyT, ActionT, Param, RoutePattern, Options, File)
@@ -30,12 +30,12 @@ scottyServer = do
 routes :: ScottyM()
 routes = do get "/service" responseService
             get "/author" responseName
-            get "/users" responseUsers
-            get "/user/id/:id" responseUserById
-            get "/user/name/:name" responseUserByName
-            post "/user/" createUser
-            put "/user/" updateUser
-            delete "/users/:id" deleteById
+--            get "/users" responseUsers
+--            get "/user/id/:id" responseUserById
+--            get "/user/name/:name" responseUserByName
+--            post "/user/" createUser
+--            put "/user/" updateUser
+--            delete "/users/:id" deleteById
 
 {-| We use [text] operator from scotty we render the response in text/plain-}
 responseService :: ActionM ()
@@ -44,51 +44,51 @@ responseService = text "First Haskell service 1.0"
 responseName :: ActionM ()
 responseName = text "Paul Perez Garcia"
 
-{-| Thanks to Aeson library and encode, we can use [json] operator to allow us to encode object into json
-    [liftAndCatchIO] operator is used to extract from the IO monad the type and add it to ActionM monad.|-}
-responseUsers :: ActionM ()
-responseUsers = do liftIO $ print ("Request received")
-                   users <- liftAndCatchIO $ getAllUsers
-                   json (show users)
-
-responseUserByName :: ActionM ()
-responseUserByName = do name <- param "name"
-                        user <- liftAndCatchIO $ getUserByUserName name
-                        json user
-
-{-| In scotty we have [param] operator which used passing the uri param name we can extract the value. -}
-responseUserById :: ActionM ()
-responseUserById = do id <- param "id"
-                      user <- liftAndCatchIO $ getUserById id
-                      json user
-
-{-| This part of the program is really interested, we are using function where first we need to call insertUser
-    passing a [User] but we have a [Maybe User] so we use a functor [<*>] to extract the User from the Maybe.
-     Then we have [sequence] operator which does:
-    -- | Evaluate each monadic action in the structure from left to right, and collect the results.
-    Then finally we need to lift the response from insertUser  [IO OK] to [OK] and to do that we use
-    the operator [liftAndCatchIO] which does:
-    -- | Like 'liftIO', but catch any IO exceptions and turn them into Scotty exceptions.
--}
-createUser :: ActionM ()
-createUser =  do maybeUser <- getUserParam
-                 status <- liftAndCatchIO $ sequence $ insertUser <$> maybeUser
-                 json (show status)
-
-updateUser :: ActionM ()
-updateUser =  do maybeUser <- getUserParam
-                 status <- liftAndCatchIO $ sequence $ updateUserById <$> maybeUser
-                 json (show status)
-
-deleteById :: ActionM ()
-deleteById = do id <- param "id"
-                status <- liftAndCatchIO $ deleteUserById id
-                json (show status)
-
-{-| In scotty we have [body] operator to get the request body.
-    We also use [decode] operator to extract and transform from json to Maybe of type we specify in the type signature-}
-getUserParam :: ActionT Text IO (Maybe User)
-getUserParam = do requestBody <- body
-                  return (decode requestBody)
+--{-| Thanks to Aeson library and encode, we can use [json] operator to allow us to encode object into json
+--    [liftAndCatchIO] operator is used to extract from the IO monad the type and add it to ActionM monad.|-}
+--responseUsers :: ActionM ()
+--responseUsers = do liftIO $ print ("Request received")
+--                   users <- liftAndCatchIO $ getAllUsers
+--                   json (show users)
+--
+--responseUserByName :: ActionM ()
+--responseUserByName = do name <- param "name"
+--                        user <- liftAndCatchIO $ getUserByUserName name
+--                        json user
+--
+--{-| In scotty we have [param] operator which used passing the uri param name we can extract the value. -}
+--responseUserById :: ActionM ()
+--responseUserById = do id <- param "id"
+--                      user <- liftAndCatchIO $ getUserById id
+--                      json user
+--
+--{-| This part of the program is really interested, we are using function where first we need to call insertUser
+--    passing a [User] but we have a [Maybe User] so we use a functor [<*>] to extract the User from the Maybe.
+--     Then we have [sequence] operator which does:
+--    -- | Evaluate each monadic action in the structure from left to right, and collect the results.
+--    Then finally we need to lift the response from insertUser  [IO OK] to [OK] and to do that we use
+--    the operator [liftAndCatchIO] which does:
+--    -- | Like 'liftIO', but catch any IO exceptions and turn them into Scotty exceptions.
+---}
+--createUser :: ActionM ()
+--createUser =  do maybeUser <- getUserParam
+--                 status <- liftAndCatchIO $ sequence $ insertUser <$> maybeUser
+--                 json (show status)
+--
+--updateUser :: ActionM ()
+--updateUser =  do maybeUser <- getUserParam
+--                 status <- liftAndCatchIO $ sequence $ updateUserById <$> maybeUser
+--                 json (show status)
+--
+--deleteById :: ActionM ()
+--deleteById = do id <- param "id"
+--                status <- liftAndCatchIO $ deleteUserById id
+--                json (show status)
+--
+--{-| In scotty we have [body] operator to get the request body.
+--    We also use [decode] operator to extract and transform from json to Maybe of type we specify in the type signature-}
+--getUserParam :: ActionT Text IO (Maybe User)
+--getUserParam = do requestBody <- body
+--                  return (decode requestBody)
 
 
