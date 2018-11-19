2888dcdecdd0cb0a9efebfe1f2f2d656816ebeee
politrons <politrons@gmail.com>
2018-11-19 19:51:47 +0100

    Add status code

--- src/main/eta/ScottyHttpServer.hs
+++ src/main/eta/ScottyHttpServer.hs
@@ -15,53 +15,56 @@ import Text.Read (lift)
 import Network.HTTP.Types.Status
 import Data.IORef (newIORef,IORef,atomicModifyIORef,readIORef,writeIORef)
 import ModelTypes
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.ByteString.Lazy.Char8 (ByteString)
 
 port = 3000 :: Int
 
 {-| Thanks to type class we define that any [User] is JSON serializable/deserializable.|-}
-instance ToJSON User
-instance FromJSON User
+--instance ToJSON String
+--instance FromJSON String
 
 {-| Using [scotty] passing [port] and [routes] we define the http server-}
 scottyServer :: IO ()
 scottyServer = do
     print ("Starting Server at port " ++ show port)
-    status <- newIORef 200 -- We create default response status
-    scotty port (routes status)
+    statusRef <- newIORef 200 -- We create default response status
+    responseBodyRef <- newIORef "{}"
+    scotty port (routes statusRef responseBodyRef)
 
 {-| We define the routes thanks to REST operators [get, post, put, delete, patch] which expect to
     receive a [RoutePattern] as a path and a [ActionM] as the action of the request. Then we return a [ScottyM]-}
-routes :: IORef Int -> ScottyM()
-routes status = do get "/service" responseService
-                   get "/author" responseName
-                   get "/mock/endpoint" (responseUsers status)
-                   get "/error" errorResponse
-                   get "/errorJson" errorJsonResponse
-                   get "/setStatus/:id" (statusResponse status)
---            post "/mock/endpoint" createUser
---            put "/mock/endpoint" updateUser
+routes :: IORef Int -> IORef String -> ScottyM()
+routes statusRef responseBodyRef= do  get "/service" responseService
+                                      get "/author" responseName
+                                      get "/mock/endpoint" (responseUsers statusRef responseBodyRef)
+                                      get "/error" errorResponse
+                                      get "/errorJson" errorJsonResponse
+                                      put "/setStatus/:id" (statusResponse statusRef responseBodyRef)
 
 {-| We use [text] operator from scotty we render the response in text/plain-}
 responseService :: ActionM ()
-responseService = text "Mock server running over JVM"
+responseService = text "Haskell Mock server running over JVM"
 
 responseName :: ActionM ()
 responseName = text "Paul Perez Garcia"
 
 {-| Thanks to Aeson library and encode, we can use [json] operator to allow us to encode object into json
     [liftAndCatchIO] operator is used to extract from the IO monad the type and add it to ActionM monad.|-}
-responseUsers :: IORef Int -> ActionM ()
-responseUsers status = do liftIO $ print ("Request received")
-                          statusCode <- liftAndCatchIO $ readIORef status
-                          users <- liftAndCatchIO $ return $ [(User statusCode "Paul")]
-                          json (show users)
+responseUsers :: IORef Int -> IORef String -> ActionM ()
+responseUsers statusRef responseBodyRef = do liftIO $ print ("Request received")
+                                             status <- liftAndCatchIO $ readIORef statusRef
+                                             responseBody <- liftAndCatchIO $ readIORef responseBodyRef
+                                             users <- liftAndCatchIO $ return $ [(User status  responseBody)]
+                                             json (show users)
 
 {-| We change the value of status for the futures request.-}
-statusResponse :: IORef Int -> ActionM ()
-statusResponse status = do uriStatus <- extractUriParam "id"
-                           liftIO (writeIORef status uriStatus)
-                           newStatus <- liftAndCatchIO $ readIORef status
-                           Web.Scotty.status status200 >> text "Status change successfully"
+statusResponse :: IORef Int -> IORef String -> ActionM ()
+statusResponse statusRef responseBodyRef = do uriStatus <- extractUriParam "id"
+                                              requestBody <- getBodyParam
+                                              liftIO (writeIORef statusRef uriStatus)
+                                              liftIO (writeIORef responseBodyRef requestBody)
+                                              Web.Scotty.status status200 >> text "Mock server change successfully"
 
 {-| To get a http response we use the function [Web.Scotty.status] passing a [Status] in this example a [status500]
     Then we use the applicative [>>] which sequentially compose two actions, passing any value produced  by the first
@@ -76,33 +79,16 @@ errorJsonResponse = do liftIO $ print ("Request received")
                        users <- liftAndCatchIO $ return $ [(User 1 "Paul")]
                        Web.Scotty.status status401 >> json (show users)
 
+{-| Function to get uriParams from the uri request-}
 extractUriParam :: Text -> ActionM Int
 extractUriParam uriParam = Web.Scotty.param uriParam
 
---{-| This part of the program is really interested, we are using function where first we need to call insertUser
---    passing a [User] but we have a [Maybe User] so we use a functor [<*>] to extract the User from the Maybe.
---     Then we have [sequence] operator which does:
---    -- | Evaluate each monadic action in the structure from left to right, and collect the results.
---    Then finally we need to lift the response from insertUser  [IO OK] to [OK] and to do that we use
---    the operator [liftAndCatchIO] which does:
---    -- | Like 'liftIO', but catch any IO exceptions and turn them into Scotty exceptions.
----}
---createUser :: ActionM ()
---createUser =  do maybeUser <- getUserParam
---                 status <- liftAndCatchIO $ sequence $ insertUser <$> maybeUser
---                 json (show status)
---
---updateUser :: ActionM ()
---updateUser =  do maybeUser <- getUserParam
---                 status <- liftAndCatchIO $ sequence $ updateUserById <$> maybeUser
---                 json (show status)
---
+{-| In scotty we have [body] operator to get the request body.
+    We also use [decode] operator to extract and transform from json to Maybe of type we specify in the type signature-}
+getBodyParam :: ActionT Text IO String
+getBodyParam = do requestBody <- body
+                  body <- (liftAndCatchIO $ return requestBody)
+                  return $ unpack body
 
---
---{-| In scotty we have [body] operator to get the request body.
---    We also use [decode] operator to extract and transform from json to Maybe of type we specify in the type signature-}
---getUserParam :: ActionT Text IO (Maybe User)
---getUserParam = do requestBody <- body
---                  return (decode requestBody)
 
 
